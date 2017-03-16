2c4a121b7189f9199db608d224596958561d5967
senseieder <senseieder@gmail.com>
2017-03-16 15:49:33 -0400

    Add files via upload

---
+++ core/core.php
@@ -0,0 +1,11 @@
+<?php
+/*Nucleo de la aplicacion*/
+
+define('HTML_DIR','html/');
+define('APP_TITLE','Ocredb/');
+define('APP_COPY', 'Todos los derechos reservados/'. date('Y',time()). 'ocrend Soft.' );
+define('APP_URL','http://localhost/sistema/');
+
+require('vendor/autoload.php');
+
+ ?>
